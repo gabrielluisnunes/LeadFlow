ce4ecee32677ca626f6d454d72068be3df9bf4b4
gabrielluisnunes <gabrielluisnunes@gmail.com>
2026-02-04 13:26:33 -0300

    feat(auth): add register auth

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -4,4 +4,20 @@ import {registerSchema, loginSchema} from "./auth.schemas.js";
 
 export async function authRoutes(app: FastifyInstance) {
   const authService = new AuthService();
+
+    app.post('/register', async (request, reply) => {
+    const body = registerSchema.parse(request.body)
+
+    const result = await authService.register(body)
+
+    const token = app.jwt.sign({
+      userId: result.user.id,
+      workspaceId: result.workspace.id
+    })
+
+    return reply.code(201).send({
+      token
+    })
+  })
+
 } 
