575a9141882d7497e3454aa5bb9c262695b2f56f
gabrielluisnunes <gabrielluisnunes@gmail.com>
2026-02-04 19:48:26 -0300

    chore(types): add jwt payload typing

--- src/types/fastify.d.ts
+++ src/types/fastify.d.ts
@@ -1,4 +1,14 @@
 import 'fastify'
+import '@fastify/jwt'
+
+declare module '@fastify/jwt' {
+    interface FastifyJWT {
+        payload: {
+            userId: string
+            workspaceId: string
+        }
+    }
+}
 
 declare module 'fastify' {
     interface FastifyInstance {
