4c6ed90fd79ec9c46e0297aef08ea5fd572697e9
gabrielluisnunes <gabrielluisnunes@gmail.com>
2026-02-05 20:33:54 -0300

    feat(leads): add protected leads routes

---
+++ src/modules/leads/leads.routes.ts
@@ -0,0 +1,29 @@
+import { FastifyInstance } from "fastify";
+import { LeadsService } from "./leads.service.js";
+import { createLeadSchema } from "./leads.schemas.js"; 
+
+export async function leadsRoutes(app: FastifyInstance) {
+    const leadsService = new LeadsService();
+
+    app.addHook("preHandler", app.authenticate); 
+
+    app.post('/', async(request, replyy) => {
+        const body = createLeadSchema.parse(request.body);
+
+        const workspaceId = request.user.workspaceId;
+        const lead = await leadsService.create({
+            workspaceId,
+            ...body
+        });
+
+        return replyy.code(201).send(lead);
+    }) 
+
+        app.get('/', async(request) => {
+            const workspaceId = request.user.workspaceId;
+            const leads = await leadsService.listByWorkspaceId(workspaceId);
+    
+            return leads;   
+            
+        });
+    }
