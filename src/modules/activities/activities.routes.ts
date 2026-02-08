8145ed81ba5cb591cd1bfa28a1a75b21bdbafab2
gabrielluisnunes <gabrielluisnunes@gmail.com>
2026-02-08 11:12:33 -0300

    feat(activities): add list activities route

---
+++ src/modules/activities/activities.routes.ts
@@ -0,0 +1,17 @@
+import { FastifyInstance } from 'fastify'
+import { ActivitiesService } from './activities.service.js'
+
+export async function activitiesRoutes(app: FastifyInstance) {
+
+  const activitiesService = new ActivitiesService()
+
+  app.addHook('preHandler', app.authenticate)
+
+  app.get('/', async (request) => {
+    const workspaceId = request.user.workspaceId
+
+    const activities = await activitiesService.listByWorkspace(workspaceId)
+
+    return activities
+  })
+}
